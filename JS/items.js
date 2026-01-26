4d8c7842d45e8fb616cc6df0c8d8e4c2a1f0c1bb
shishirgautam939-tech <shishirgautam939@gmail.com>
2026-01-26 17:12:18 +0545

    code written in items.js

---
+++ JS/items.js
@@ -0,0 +1,14 @@
+import { createSingleItem } from "JS/single-item.js";
+
+
+export function createItems(itemsArray) {
+  const container = document.createElement("div");
+  container.className = "items";
+
+  itemsArray.forEach((item) => {
+    const itemElement = createSingleItem(item);
+    container.appendChild(itemElement);
+  });
+
+  return container;
+}
