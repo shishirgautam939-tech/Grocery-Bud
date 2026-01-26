7d5f6e195cd5563fa5eeea648b25eda525b5a089
shishirgautam939-tech <shishirgautam939@gmail.com>
2026-01-26 20:24:15 +0545

    update singleitem.js

--- JS/single-item.js
+++ JS/single-item.js
@@ -1,3 +1,5 @@
+import { editCompleted } from "./app.js";
+
 export function createSingleItem(item) {
   const div = document.createElement("div");
   div.className = "single-item";
@@ -14,6 +16,11 @@ export function createSingleItem(item) {
       <i class="fa-regular fa-trash-can"></i>
     </button>
   `;
+
+  const checkbox = div.querySelector('input[type="checkbox"]');
+  checkbox.addEventListener("change", () => {
+    editCompleted(item.id);
+  });
 
   return div;
 }
