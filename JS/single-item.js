22af8d346b6b844dc3b20c4dea83b36a27c48069
shishirgautam939-tech <shishirgautam939@gmail.com>
2026-01-29 09:48:24 +0545

    update singleitem.js,now we can edit grocery names

--- JS/single-item.js
+++ JS/single-item.js
@@ -1,4 +1,4 @@
-import { editCompleted, removeItem } from "./app.js";
+import { editCompleted, removeItem, setEditId } from "./app.js";
 
 export function createSingleItem(item) {
   const div = document.createElement("div");
@@ -17,16 +17,19 @@ export function createSingleItem(item) {
     </button>
   `;
 
-  
   const checkbox = div.querySelector('input[type="checkbox"]');
   checkbox.addEventListener("change", () => {
     editCompleted(item.id);
   });
 
- 
   const removeBtn = div.querySelector(".remove-btn");
   removeBtn.addEventListener("click", () => {
     removeItem(item.id);
+  });
+
+  const editBtn = div.querySelector(".edit-btn");
+  editBtn.addEventListener("click", () => {
+    setEditId(item.id);
   });
 
   return div;
